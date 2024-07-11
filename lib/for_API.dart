c2c8155b7440e4721583a78536dc3e03e40e84f7
nahomsof <nahomsileshi21@gmail.com>
2024-07-11 12:11:23 +0300

    commit

--- lib/for_API.dart
+++ lib/for_API.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,6 +9,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var userId;
+  var title;
+  var completed;
+
+    getData() async{
+      var url = Uri.https('https://jsonplaceholder.typicode.com/todos/1');
+      http.Response response = await http.get(url);
+      print(response);
+
+    }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
