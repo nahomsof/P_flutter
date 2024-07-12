9f777b8fecd969b59e6683d8d62f95331100178a
nahomsof <nahomsileshi21@gmail.com>
2024-07-12 10:25:20 +0300

    Comit

--- lib/OtherApi.dart
+++ lib/OtherApi.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -6,22 +7,41 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  fetchdata() async {
+    var url = Uri.parse('https://jsonplaceholder.typicode.com/posts/');
+    var response = await http.get(url);
+    if (response == 200) {
+      print("Data fetched succussfully: ${response.body}");
+    }
+  }
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-            home: Scaffold(
-        appBar: AppBar(title: Text("HTTP Requiest in Flutter"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("HTTP Requiest in Flutter"),
         ),
-        body: Center(child: Column(children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [    
-          ElevatedButton(onPressed: (){}, child: Text("Fetch Data")),
-          ElevatedButton(onPressed: (){}, child: Text("Create Data")),
-          ElevatedButton(onPressed: (){}, child: Text("Delete Data"))
-          ],),
-          Expanded(child: SingleChildScrollView(child: Text("")))
-        ],),),
+        body: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        fetchdata();
+                      },
+                      child: Text("Fetch Data")),
+                  ElevatedButton(onPressed: () {}, child: Text("Create Data")),
+                  ElevatedButton(onPressed: () {}, child: Text("Delete Data"))
+                ],
+              ),
+              Expanded(child: SingleChildScrollView(child: Text("Some data")))
+            ],
+          ),
+        ),
       ),
     );
   }
