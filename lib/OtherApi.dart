8bfe5c942a74f8d5ca368c6b2a6701bf5a26a52e
nahomsof <nahomsileshi21@gmail.com>
2024-07-20 23:57:57 +0300

    commit

--- lib/OtherApi.dart
+++ lib/OtherApi.dart
@@ -42,7 +42,7 @@ class _MyAppState extends State<MyApp> {
               TextFormField(
                 textAlign: TextAlign.center,
                 onChanged: (value) {
-                  if (value != null) {
+                  if (value != ) {
                     type = value;
                   }
                 },
