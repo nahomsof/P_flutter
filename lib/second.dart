0fa5f720542131404c20d25211f27d5538f62033
nahomsof <nahomsileshi21@gmail.com>
2024-07-31 18:40:22 +0300

    commit

--- lib/second.dart
+++ lib/second.dart
@@ -1,13 +1,19 @@
 import 'dart:async';
 
-void main() {
+void main() async {
   int b = 5;
   int a = 6;
-  final total = getTotal(a, b);
+  double c = 3.142434525452452;
+  bool bolean;
+  String tostring = b.toString();
+  String d = c.toStringAsFixed(2);
+  assert(b == "5");
+  final total = await getTotal(a, b);
   print(total);
+  print(d);
+  (tostring == "5") ? print("True") : print("Fales");
 }
 
 Future<int> getTotal(a, b) {
   return Future.delayed(Duration(seconds: 2), () => a + b);
-
 }
